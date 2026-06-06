5c9d674e6cf9a9cfeef626e83ff93683cb74cff8
Caspar Kroll <ckroll@schulzentrum.de>
2026-06-06 09:52:30 +0200

    Expansion board v0.2

--- js/expansion.js
+++ js/expansion.js
@@ -16,7 +16,7 @@ function getCP1() {
 
 function getCP2() {
     if (J9) {
-        return (ai2 < DAC(org2) ? 0 : 1) 
+        return (Math.max(ai2, temp) < DAC(org2) ? 0 : 1) // Jumper 9 connects the temperature sensor to same wire as AI2, so the CP2 comparator compares the higher of the two voltages to the DAC output
     } else {
         return (temp < DAC(org2) ? 0 : 1) 
     }
@@ -58,20 +58,35 @@ function DAC(value) {
 }
 
 function display() {
-    document.querySelectorAll(`svg .org1`).forEach(el => el.textContent = org1.toString(2).padStart(8, "0"));
-    document.querySelectorAll(`svg .ao1-v`).forEach(el => el.textContent = DAC(org1).toFixed(2) + "V");
-    document.querySelectorAll(`svg .org2`).forEach(el => el.textContent = org2.toString(2).padStart(8, "0"));
-    document.querySelectorAll(`svg .ao2-v`).forEach(el => el.textContent = DAC(org2).toFixed(2) + "V");
-    document.querySelectorAll(`svg .irg`).forEach(el => el.textContent = irg.toString(2).padStart(8, "0"));
+    document.querySelectorAll(`.org1`).forEach(el => el.textContent = org1.toString(2).padStart(8, "0"));
+    document.querySelectorAll(`.ao1-v`).forEach(el => el.textContent = DAC(org1).toFixed(2) + "V");
+    document.querySelectorAll(`.ai1`).forEach(el => el.value = ai1.toFixed(2));
+    document.querySelectorAll(`.org2`).forEach(el => el.textContent = org2.toString(2).padStart(8, "0"));
+    document.querySelectorAll(`.ao2-v`).forEach(el => el.textContent = DAC(org2).toFixed(2) + "V");
+    document.querySelectorAll(`.ai2`).forEach(el => el.value = ai2.toFixed(2));
+    document.querySelectorAll(`.temp`).forEach(el => el.value = temp.toFixed(2));
+    document.querySelectorAll(`.uio`).forEach(el => el.value = uioReg.toString(2).padStart(3, "0"));
+    document.querySelectorAll(`.irg`).forEach(el => el.value = irg.toString(2).padStart(8, "0"));
 
-    document.querySelectorAll(`svg .cp1`).forEach(el => el.setAttribute("fill", (getCP1() ? "yellow" : "slategray")));
-    document.querySelectorAll(`svg .cp2`).forEach(el => el.setAttribute("fill", (getCP2() ? "yellow" : "slategray")));
-    document.querySelectorAll(`svg .ao1`).forEach(el => el.setAttribute("fill", "#" + Math.round(mapBrightness(DAC(org1))).toString(16).padStart(2, "0") + "0000"));
-    document.querySelectorAll(`svg .ao2`).forEach(el => el.setAttribute("fill", "#" + Math.round(mapBrightness(DAC(org2))).toString(16).padStart(2, "0") + "0000"));
+    document.querySelectorAll(`.cp1`).forEach(el => el.setAttribute("fill", (getCP1() ? "yellow" : "slategray")));
+    document.querySelectorAll(`.cp2`).forEach(el => el.setAttribute("fill", (getCP2() ? "yellow" : "slategray")));
+    document.querySelectorAll(`.ao1`).forEach(el => el.setAttribute("fill", "#" + Math.round(mapBrightness(DAC(org1))).toString(16).padStart(2, "0") + "0000"));
+    document.querySelectorAll(`.ao2`).forEach(el => el.setAttribute("fill", "#" + Math.round(mapBrightness(DAC(org2))).toString(16).padStart(2, "0") + "0000"));
+    document.querySelectorAll(`.ai2`).forEach(el => el.setAttribute("fill", "#" + Math.round(mapBrightness(J9 ? Math.max(ai2, temp) : ai2)).toString(16).padStart(2, "0") + "0000"));
+
+    document.querySelectorAll(`#fan-con svg #Rotor`).forEach(el => el.style.animation = `spin ${mapFanSpeed(DAC(org1))}s linear infinite`);
 }
 
 function mapBrightness(number, inMin=0, inMax=2.55, outMin=0x00, outMax=0xFF) {
+    number = Math.min(Math.max(number, inMin), inMax);
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+function mapFanSpeed(number) {
+    if (number < 0.5) {
+        return 0;
+    }
+    return mapBrightness(number, 0.5, 2.55, 100, 0);
 }
 
 setInterval(() => {
